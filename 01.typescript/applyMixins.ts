c5f403dab89e72509ef3321c65f66de1db24c6f1
t2 <oo_ri@naver.com>
2021-06-06 02:20:54 +0900

    Mixin Example Code Update

---
+++ 01.typescript/applyMixins.ts
@@ -0,0 +1,17 @@
+// deno-lint-ignore no-explicit-any
+function applyMixins(derivedCtor: any, baseCtors: any[]) {
+  baseCtors.forEach(baseCtor => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+      const baseCtorName = Object.getOwnPropertyDescriptor(
+        baseCtor.prototype,
+        name
+      );
+
+      if (!baseCtorName) return;
+
+      Object.defineProperty(derivedCtor.prototype, name, baseCtorName);
+    });
+  });
+}
+
+export default applyMixins;
