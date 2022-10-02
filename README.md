72fe38e5e7c6e9ff832e2e26af45998ad84856c4
dev-t2 <oo_ri@naver.com>
2022-10-02 13:16:56 +0900

    Create, Read and Update API Update

--- README.md
+++ README.md
@@ -19,3 +19,5 @@
   - [Connect your database](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/connect-your-database-typescript-postgres)
   - [Using Prisma Migrate](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/using-prisma-migrate-typescript-postgres)
   - [Referential actions](https://www.prisma.io/docs/concepts/components/prisma-schema/relations/referential-actions)
+  - [Install Prisma Client](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/install-prisma-client-typescript-postgres)
+  - [Querying the database](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/querying-the-database-typescript-postgres)
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,46 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
 app.set('port', 3000);
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.send('Hello Express');
+const prisma = new PrismaClient();
+
+interface ICreateUserRequest extends Request {
+  body: { email: string; nickname: string; password: string };
+}
+
+app.post('/', async (req: ICreateUserRequest, res) => {
+  const { email, nickname, password } = req.body;
+
+  const user = await prisma.user.create({ data: { email, nickname, password } });
+
+  return res.json(user);
+});
+
+app.get('/', async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  return res.json(users);
+});
+
+interface IUpdateUserRequest extends Request {
+  params: { id: string };
+  body: { nickname: string };
+}
+
+app.put('/:id', async (req: IUpdateUserRequest, res) => {
+  const { id } = req.params;
+  const { nickname } = req.body;
+
+  const users = await prisma.user.update({ where: { id: Number(id) }, data: { nickname } });
+
+  return res.json(users);
 });
 
 app.use((req, res) => {
