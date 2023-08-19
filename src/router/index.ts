a4d85da076b95b20b6c3efb4fd77757adcfa0ec0
dev-t2 <oo_ri@naver.com>
2023-08-19 16:27:07 +0900

    Go Lottery Repository Reset

--- src/router/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as fakers } from './fakers';
-export { default as hashtags } from './hashtags';
-export { default as posts } from './posts';
-export { default as users } from './users';
